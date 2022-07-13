59430022404a105aa732a191756a290b63ecaf0e
Samuel Banya <sbanya@fastmail.com>
2022-07-12 23:03:39 -0500

    commit tree c9043e4cf1e46aa5e9dc3b97913a55835cd21cc5

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDeleteQuestion, onUpdateQuestion }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,15 +9,52 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleDeleteClick(e) {
+    console.log("handleDeleteClick() function called");
+    e.preventDefault();
+
+    console.log("id: ", id);
+
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then(() => onDeleteQuestion(question));
+  }
+
+  function handleQuestionChange(e) {
+    console.log("handleQuestionChange() function called");
+    e.preventDefault();
+    console.log("e.target.value: ", e.target.value);
+    const newCorrectAnswer = e.target.value;
+
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        correctIndex: newCorrectAnswer,
+      }),
+    })
+      .then((response) => response.json())
+      .then((updatedQuestion) => {
+        onUpdateQuestion(updatedQuestion);
+        console.log("updatedQuestion: ", updatedQuestion);
+      });
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select onChange={handleQuestionChange} defaultValue={correctIndex}>
+          {options}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteClick}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  let [questions, setQuestions] = useState([]);
+  // Somehow, I need to probably use 'useState' to store the state accordingly for later use in case the
+  // the question items change
+  // Or, I just need to use 'useEffect()' to piggyback off of a page load:
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((data) => {
+        questions = data;
+        // console.log("data: ", data);
+        setQuestions(questions);
+      });
+  }, []);
+
+  function handleDeleteQuestion(deletedQuestion) {
+    console.log(
+      "Deleted Question (from handleDeleteItem() in QuestionList component: )",
+      deletedQuestion
+    );
+    const updatedQuestions = questions.filter(
+      (question) => question.id !== deletedQuestion.id
+    );
+    setQuestions(updatedQuestions);
+  }
+
+  function handleUpdateQuestion(updatedQuestion) {
+    console.log("handleUpdateQuestion() called");
+    const updatedQuestions = questions.map((question) => {
+      if (question.id === updatedQuestion.id) {
+        return updatedQuestion;
+      } else {
+        return question;
+      }
+    });
+    setQuestions(updatedQuestions);
+  }
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions
+          ? questions.map((question) => {
+              // {id: 1, prompt: 'What special prop should always be included for lists of elements?', answers: Array(4), correctIndex: 2}
+              {
+                console.log("From QuestionList component: ");
+                console.log(question);
+              }
+              return (
+                <QuestionItem
+                  key={question["id"]}
+                  question={question}
+                  onDeleteQuestion={handleDeleteQuestion}
+                  onUpdateQuestion={handleUpdateQuestion}
+                />
+              );
+            })
+          : null}
+      </ul>
     </section>
   );
 }
